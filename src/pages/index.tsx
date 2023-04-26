6041b2ba3a3d03b2ffb008c16792d4537e96051a
Davi Caju <devcaju@gmail.com>
2023-04-26 20:00:31 -0300

    feat: add and configure stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,56 @@
-import { styled } from "@/styles";
+import Image from "next/image";
 
-const Button = styled("button", {
-  background: "$green500",
-});
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import { useKeenSlider } from "keen-slider/react";
+
+import tShirtImage from "../assets/tshirts/1.png";
+import tShirtImage2 from "../assets/tshirts/2.png";
+import tShirtImage3 from "../assets/tshirts/3.png";
+
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
-  return <Button>Send</Button>;
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={tShirtImage} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>T Shirt</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={tShirtImage2} width={520} height={480} alt="" />
+        <footer>
+          <strong>T Shirt</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tShirtImage3} width={520} height={480} alt="" />
+        <footer>
+          <strong>T Shirt</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tShirtImage3} width={520} height={480} alt="" />
+        <footer>
+          <strong>T Shirt</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,7 @@
-export function Product() {
-  return <h1>Product</h1>;
+import { useRouter } from "next/router";
+
+export default function Product() {
+  const { query } = useRouter();
+
+  return <h1>Product: {JSON.stringify(query)}</h1>;
 }
